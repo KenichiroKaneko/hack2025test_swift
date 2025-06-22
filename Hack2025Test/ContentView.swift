63c6d2811c908810dd3cccf44d50a9766723ccd1
KanekoKenichiro <fanta.remio.ken@gmail.com>
2025-06-23 00:22:46 +0900

    initial commit

--- Hack2025Test/ContentView.swift
+++ Hack2025Test/ContentView.swift
@@ -7,55 +7,31 @@
 
 import SwiftUI
 import SwiftData
+//import SwiftUe
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        NavigationStack {
+            HStack {
+                navigationButtonView(destination: ContentView(), label: "Controller")
+                navigationButtonView(destination: ContentView(), label: "Camera")
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            .frame(maxWidth: .infinity)
+            .background(Color.gray)
+            
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+    
+    func navigationButtonView<Destination: View>(
+        destination: Destination, label: String) -> some View {
+            NavigationLink(destination: destination) {
+                Text(label)
+                    .padding(8)
+                    .background(in: RoundedRectangle(cornerRadius: 30))
             }
         }
-    }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
