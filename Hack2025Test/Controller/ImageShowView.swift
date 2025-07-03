d02dbd3da16777abea91b648bd1f3d2083f364bc
KenichiroKaneko <fanta.remio.ken@gmail.com>
2025-07-03 16:09:14 +0900

    絵文字の追加

--- Hack2025Test/Controller/ImageShowView.swift
+++ Hack2025Test/Controller/ImageShowView.swift
@@ -4,32 +4,50 @@ import SwiftUI
 struct ImageShowView: View {
     @Binding var showImageView: Bool
     @ObservedObject var receiver: WebSocketReceiver  // 外から受け取る
-
+    
     @State private var showCloseConfirmation = false
-
+    
     var body: some View {
         ZStack {
+            
             Color.gray.ignoresSafeArea()
-
+            
             VStack {
-                ScrollView {
-                    LazyVGrid(columns: [GridItem(), GridItem()], spacing: 10) {
+                VStack {
+                    LazyVGrid(columns: [GridItem(), GridItem()], spacing: 0) {
                         ForEach(receiver.images.indices, id: \.self) { index in
+                            
                             Image(uiImage: receiver.images[index])
                                 .resizable()
                                 .scaledToFit()
-                                .frame(height: 150)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .background(.white)
+                                .padding(16)
                                 .cornerRadius(8)
+                                .overlay(alignment: .bottomTrailing){
+                                    Text(receiver.emojis[index])
+                                        .font(.system(size: 80))
+                                        .padding(8)
+                                        .background(
+                                            Circle()
+                                                .fill(Color.white)
+                                            
+                                        )
+                                    
+                                    
+                                }
                         }
                     }
-                    .padding()
+                    .padding(64)
+                    
                 }
-
                 Button("閉じる") {
                     showCloseConfirmation = true
                 }
                 .padding(.bottom)
             }
+            
+            
         }
         .confirmationDialog("本当に閉じますか？", isPresented: $showCloseConfirmation) {
             Button("閉じる", role: .destructive) {
@@ -41,7 +59,23 @@ struct ImageShowView: View {
 }
 
 
+class DummyReceiver: WebSocketReceiver {
+    override init() {
+        super.init()
+        self.images = [
+            UIImage(systemName: "person.crop.square")!,
+            UIImage(systemName: "person.crop.square")!,
+            UIImage(systemName: "person.crop.square")!,
+            UIImage(systemName: "person.crop.square")!,
+            UIImage(systemName: "star")!,
+            UIImage(systemName: "heart")!
+        ]
+        self.emojis = [
+            "😍", "😳", "😭", "😮", "💖", "⭐️"
+        ]
+    }
+}
 
 #Preview {
-//    ImageShowView()
+    ImageShowView(showImageView: .constant(true), receiver: DummyReceiver())
 }
